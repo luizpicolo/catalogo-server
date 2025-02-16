f9b133eca00379329c89b85a68d46fb7d3f71758
Luiz Picolo <luizpicolo@gmail.com>
2025-02-15 20:45:59 -0400

    adiciona novo aplicativo "Storage" à lista de aplicações

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const applications = [
   { name: "Picolo Social", url: "https://social.luizpicolo.com.br" },
   { name: "Tranferência de dados", url: "https://dtt.luizpicolo.com.br" },
+  { name: "Storage", url: "https://files.luizpicolo.com.br" },
   { name: "Gitea", url: "https://git.luizpicolo.com.br" },
   { name: "Blog Pessoal", url: "https://blog.luizpicolo.com.br" },
   { name: "Portainer", url: "http://192.168.0.28:9000" },
